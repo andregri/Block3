1ea71b9cae2ae0c7734353097deaecb28c3abed6
andregri <andrea.grillo96@live.com>
2016-10-22 13:29:59 +0200

    create vs solution with test project

---
+++ Block3/UnitTest/UnitTest02.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest02
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
