48f3603b835e4420aa09d1883678cef233ba0d41
andregri <andrea.grillo96@live.com>
2016-10-25 18:22:40 +0200

    add greaterThanNeighbour function with test

--- Block3/Exercise01/Program.cs
+++ Block3/Exercise01/Program.cs
@@ -8,5 +8,32 @@ namespace Exercise01
         static void Main(string[] args)
         {
         }
+
+        public static bool GreaterThanNeighbours(int[] array, int index)
+        {
+            int length = array.Length;
+
+            if (length == 1)
+            {
+                return true;
+            }
+            else if (length > 1)
+            {
+                if (index == 0 && array[0] > array[1])
+                {
+                    return true;
+                }
+                else if (index == array.Length - 1 && array[index - 1] < array[index])
+                {
+                    return true;
+                }
+                else if (array[index] > array[index - 1] && array[index] > array[index + 1])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
--- Block3/UnitTest/UnitTest01.cs
+++ Block3/UnitTest/UnitTest01.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise01;
 
 namespace UnitTest
 {
@@ -7,8 +8,31 @@ namespace UnitTest
     public class UnitTest01
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GreaterThanNeighbours()
         {
+            bool expected = Program.GreaterThanNeighbours(new int[] { 1, 3, 2 }, 1);
+            Assert.IsTrue(expected);
+        }
+
+        [TestMethod]
+        public void GreaterThanNeighbours_ArrayOneElement()
+        {
+            bool expected = Program.GreaterThanNeighbours(new int[] { 1 }, 0);
+            Assert.IsTrue(expected);
+        }
+
+        [TestMethod]
+        public void GreaterThanNeighbours_FirstElement()
+        {
+            bool expected = Program.GreaterThanNeighbours(new int[] { 3, 2 }, 0);
+            Assert.IsTrue(expected);
+        }
+
+        [TestMethod]
+        public void GreaterThanNeighbours_LastElement()
+        {
+            bool expected = Program.GreaterThanNeighbours(new int[] { 3, 5 }, 1);
+            Assert.IsTrue(expected);
         }
     }
 }
