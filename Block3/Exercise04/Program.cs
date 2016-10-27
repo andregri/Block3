f6169eee519cbd175dea866d90228b9a60e5b68c
andregri <andrea.grillo96@live.com>
2016-10-27 23:04:19 +0200

    add holidays jagged array

--- Block3/Exercise04/Program.cs
+++ Block3/Exercise04/Program.cs
@@ -5,6 +5,22 @@ namespace Exercise04
 {
     public class Program
     {
+        public static DateTime[][] holidays = new DateTime[12][]
+        {
+            new DateTime[] {new DateTime(DateTime.Now.Year, 1, 1), new DateTime(DateTime.Now.Year, 1, 6)},
+            new DateTime[] {},
+            new DateTime[] {new DateTime(DateTime.Now.Year, 3, 27), new DateTime(DateTime.Now.Year, 3, 28)},
+            new DateTime[] {new DateTime(DateTime.Now.Year, 4, 25), new DateTime(DateTime.Now.Year, 4, 28)},
+            new DateTime[] {new DateTime(DateTime.Now.Year, 5, 1), new DateTime(DateTime.Now.Year, 1, 16)},
+            new DateTime[] {new DateTime(DateTime.Now.Year, 6, 2)},
+            new DateTime[] {},
+            new DateTime[] {new DateTime(DateTime.Now.Year, 8, 15)},
+            new DateTime[] {},
+            new DateTime[] {},
+            new DateTime[] {new DateTime(DateTime.Now.Year, 11, 1)},
+            new DateTime[] {new DateTime(DateTime.Now.Year, 12, 8), new DateTime(DateTime.Now.Year, 12, 25), new DateTime(DateTime.Now.Year, 12, 26)},
+        };
+
         static void Main(string[] args)
         {
         }
