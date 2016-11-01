ddba127049e0d0dd21389e4cfd86bed0295b84e3
michisalvo <jimmyginooo@libero.it>
2016-11-01 15:34:32 +0100

    Add whole exercise

--- Block3/Exercise03/Program.cs
+++ Block3/Exercise03/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
-
+using System.IO;
 
 namespace Exercise03
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            string path;
+
+            while (true)
+            {
+
+                Console.WriteLine("Please enter a valid path:");
+                path = Console.ReadLine();
+                Console.WriteLine("\n");
+
+                //string example =(@"/home/jarvis/Documenti/University");
+                try
+                {
+                    Directory.Exists(path);
+                    DirectorySearch(path);
+                }
+                catch (System.Exception exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+            }
+        }
+
+        static void DirectorySearch(string dir)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(dir))
+                    Console.WriteLine(file);
+
+                foreach (string directory in Directory.GetDirectories(dir))
+                {
+                    //Console.BackgroundColor = ConsoleColor.DarkCyan;
+                    //Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(directory);
+                    //Console.ResetColor();
+                    DirectorySearch(directory);
+                }
+            }
+            catch (System.Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
         }
     }
 }
