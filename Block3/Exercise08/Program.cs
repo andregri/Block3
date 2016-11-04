bcb8180b3203981c9f6b45a7fb57bc21491add28
andregri <andrea.grillo96@live.com>
2016-11-04 09:50:07 +0100

    add function to encrypt text

--- Block3/Exercise08/Program.cs
+++ Block3/Exercise08/Program.cs
@@ -8,5 +8,20 @@ namespace Exercise08
         static void Main(string[] args)
         {
         }
+
+        public static char[] EncryptText(char[] text, char[] chiper)
+        {
+            long length = text.Length;
+            char[] encrypted = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                ushort t = text[i];
+                ushort c = chiper[i % chiper.Length];
+                encrypted[i] = (char)(t ^ c);
+            }
+
+            return encrypted;
+        }
     }
 }
