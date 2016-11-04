cac73aec81d4c5a56a55ece885be98fd840e73fa
andregri <andrea.grillo96@live.com>
2016-11-04 10:17:53 +0100

    add main implementation

--- Block3/Exercise08/Program.cs
+++ Block3/Exercise08/Program.cs
@@ -7,6 +7,20 @@ namespace Exercise08
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a text, please:");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Enter a chiper, please:");
+            string chiper = Console.ReadLine();
+
+            ushort[] encrypted = EncryptText(text, chiper);
+
+            Console.WriteLine("Encrypted text is:");
+            foreach (ushort c in encrypted)
+            {
+                Console.Write("\\u{0:x4}", c);
+            }
+            Console.WriteLine();
         }
 
         public static ushort[] EncryptText(string text, string chiper)
