6d51fd7f35ddc04e2ec8ce0709e85d2bea875ab6
michisalvo <jimmyginooo@libero.it>
2016-11-05 14:41:19 +0100

    Add whole exercise, no test yet

--- Block3/Exercise10/Program.cs
+++ Block3/Exercise10/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Exercise10
 {
@@ -6,6 +8,36 @@ namespace Exercise10
     {
         static void Main(string[] args)
         {
+            string userInput;
+
+            do
+            {
+                Console.Write("Enter a string (press 'q' to exit):  ");
+                userInput = Console.ReadLine();
+                CountingLetters(userInput);
+
+            } while (userInput != "q");
+
+        }
+        public static void CountingLetters(string input)
+        {
+            SortedDictionary<char, int> dict = new SortedDictionary<char, int>();
+
+            for (int index = 0; index < input.Length; index++)
+            {
+                char key = input[index];
+
+                if (!dict.ContainsKey(key))
+                    dict.Add(key, 1);
+                else
+                    dict[input[index]]++;
+            }
+
+            foreach (var key in dict.OrderByDescending(value => value.Value).Take(315))
+            {
+                Console.WriteLine("{0}\t{1}", key.Key, key.Value);
+
+            }
         }
     }
 }
