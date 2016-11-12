2cc4b5e360b56d52746cdaaa85c9b3b3a59f11b3
andregri <andrea.grillo96@live.com>
2016-11-12 12:41:40 +0100

    add function to read integer from a file

--- Block3/Exercise06/Program.cs
+++ Block3/Exercise06/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.IO;
 
 namespace Exercise06
 {
@@ -7,6 +7,29 @@ namespace Exercise06
     {
         static void Main(string[] args)
         {
+        }
+
+        public static void ParseFile(string file)
+        {
+            StreamReader reader = null;
+            int lineNumber = 0;
+
+            try
+            {
+                using (reader = new StreamReader(file))
+                {
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        int.Parse(line);
+                    }
+                }
+            }
+            catch (FormatException e)
+            {
+                throw new FileParseException(file, lineNumber, e);
+            }
         }
     }
 }
