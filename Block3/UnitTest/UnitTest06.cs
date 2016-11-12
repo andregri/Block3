f30392ff05cc678bfdf841da76c4f1495814528f
andregri <andrea.grillo96@live.com>
2016-11-12 13:47:01 +0100

    add test for ParseFile()

--- Block3/UnitTest/UnitTest06.cs
+++ Block3/UnitTest/UnitTest06.cs
@@ -7,6 +7,8 @@ namespace UnitTest
     [TestClass]
     public class UnitTest06
     {
+        static string path = @"..\..\..\Exercise06\";
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void FileParseExceptionTestFilename()
@@ -19,6 +21,24 @@ namespace UnitTest
         public void FilePerseExceptionTestLine()
         {
             new FileParseException("file", -2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileParseException))]
+        public void ParseFileTestException()
+        {
+            string file = path + "wrong.txt";
+            Program.ParseFile(file);
+        }
+
+        [TestMethod]
+        public void ParseFileTest()
+        {
+            string file = path + "correct.txt";
+            int[] numbers = Program.ParseFile(file);
+            int[] expected = { 1, 4, 7, 8 };
+
+            CollectionAssert.AreEqual(expected, numbers);
         }
     }
 }
