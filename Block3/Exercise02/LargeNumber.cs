46396006eebcce2610c1d72a23e3f23114088915
andregri <andrea.grillo96@live.com>
2016-11-13 17:04:55 +0100

    add extend method

--- Block3/Exercise02/LargeNumber.cs
+++ Block3/Exercise02/LargeNumber.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Text;
 
 namespace Exercise02
 {
@@ -7,6 +7,30 @@ namespace Exercise02
     {
         static void Main(string[] args)
         {
+        }
+
+        // if 'num' is smaller than 'newSize', add a padding of zeroes to 'num';
+        // else return 'num' unmodified.
+        public static string Extend(string num, long newSize)
+        {
+            StringBuilder newNum;
+            long oldSize = num.Length;
+
+            if (oldSize >= newSize)
+            {
+                return num;
+            }
+            else
+            {
+                newNum = new StringBuilder(num);
+
+                for (long i = oldSize; i < newSize; i++)
+                {
+                    newNum.Append('0');
+                }
+            }
+
+            return newNum.ToString();
         }
     }
 }
--- Block3/UnitTest/UnitTest02.cs
+++ Block3/UnitTest/UnitTest02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise02;
 
 namespace UnitTest
 {
@@ -7,8 +8,26 @@ namespace UnitTest
     public class UnitTest02
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ExtendTestGreater()
         {
+            string num = "465465";
+            long size = 10;
+
+            string newNum = LargeNumber.Extend(num, size);
+            string expected = "4654650000";
+
+            Assert.AreEqual(expected, newNum);
+        }
+
+        [TestMethod]
+        public void ExtendTestSmaller()
+        {
+            string num = "76434";
+            long size = 3;
+
+            string newNum = LargeNumber.Extend(num, size);
+
+            Assert.AreEqual(num, newNum);
         }
     }
 }
