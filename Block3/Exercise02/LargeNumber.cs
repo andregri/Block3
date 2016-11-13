c2848d12fbae59acf9eece91912ba79ec0086786
andregri <andrea.grillo96@live.com>
2016-11-13 19:25:35 +0100

    add sum function

--- Block3/Exercise02/LargeNumber.cs
+++ Block3/Exercise02/LargeNumber.cs
@@ -32,5 +32,41 @@ namespace Exercise02
 
             return newNum.ToString();
         }
+
+        public static string Sum(string a, string b)
+        {
+            long sizeMax = Math.Max(a.Length, b.Length);
+            string num1 = Extend(a, sizeMax);
+            string num2 = Extend(b, sizeMax);
+
+            StringBuilder result = new StringBuilder();
+
+            //sum first digits
+            int op1 = num1[0] - 48;
+            int op2 = num2[0] - 48;
+            int partialSum = op1 + op2;
+            result.Append(partialSum % 10);
+            int carryOut = partialSum / 10;
+
+            //sum other digits
+            for (int i = 1; i < sizeMax; i++)
+            {
+                op1 = num1[i] - 48;
+                op2 = num2[i] - 48;
+                partialSum = op1 + op2 + carryOut;
+
+                result.Append(partialSum % 10);
+
+                //prepare carryOut for the next sum
+                carryOut = partialSum / 10;
+            }
+
+            if (carryOut > 0)
+            {
+                result.Append(carryOut);
+            }
+
+            return result.ToString();
+        }
     }
 }
--- Block3/UnitTest/UnitTest02.cs
+++ Block3/UnitTest/UnitTest02.cs
@@ -7,6 +7,14 @@ namespace UnitTest
     [TestClass]
     public class UnitTest02
     {
+        private string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        //test extend
         [TestMethod]
         public void ExtendTestGreater()
         {
@@ -28,6 +36,19 @@ namespace UnitTest
             string newNum = LargeNumber.Extend(num, size);
 
             Assert.AreEqual(num, newNum);
+        }
+
+        //test sum
+        [TestMethod]
+        public void SumTest1()
+        {
+            string a = "65468";
+            string b = "54687";
+
+            string expected = "120155";
+            string result = LargeNumber.Sum(Reverse(a), Reverse(b));
+
+            Assert.AreEqual(expected, Reverse(result));
         }
     }
 }
